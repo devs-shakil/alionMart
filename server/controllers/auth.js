7d974da384d3d54d2b0617a050593f3b70ed8bcc
shakilwebmaster <mdshakil102036@gmail.com>
2023-06-11 22:43:06 +0600

    login auth add

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -54,3 +54,38 @@ exports.register = async (req, res) => {
         console.log(error)
     }
 }
+
+
+
+exports.login = async (req, res) =>{
+    try {
+        //1. destructure name, email, password from req.body
+        const {email, password} = req.body;
+
+        //2. all fields require validation
+        if(!email){
+            return res.json({error: "Email is required"})
+        }
+        if(!password || password.length < 6){
+            return res.json({error: "password must be 6 characters long"})
+        }
+
+        //3. check if email is taken
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({error: "User not found"})
+        } 
+        //4. compare password
+        const match = await comparePassword(password, user.password);
+        if(!match){
+            return res.json({error: "Invalid email or password"})
+        }
+
+        //5. create sign jwt
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d", })
+
+        
+    } catch (error) {
+        
+    }
+}
